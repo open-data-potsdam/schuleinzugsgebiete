34209cfa1f985f1e0d2ba665b327951e071f8f79
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2017-01-21 22:07:07 +0100

    Create Clojure project

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject schuleinzugsgebiete "1.0.0-SNAPSHOT"
+  :description "School districts in Potsdam"
+  :url "https://github.com/open-data-potsdam/schuleinzugsgebiete"
+
+  :license {:name "Apache License version 2.0"
+            :url "http://www.apache.org/licenses/"}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]])
