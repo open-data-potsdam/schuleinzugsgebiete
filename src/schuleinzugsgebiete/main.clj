865060647bce136bed787899eaf56b4bc5e6b0dd
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2017-04-22 11:42:45 +0200

    Migrate generator to duct
    The development server can be run from the REPL, while lein run
    triggers only the static site generator. The generator itself is a
    component, that is loaded via duct and integrant.

--- src/schuleinzugsgebiete/main.clj
+++ src/schuleinzugsgebiete/main.clj
@@ -1,7 +1,21 @@
 (ns schuleinzugsgebiete.main
     (:gen-class)
-    (:require [clojure.java.io :as io]
-              [duct.core :as duct]))
+    (:require [schuleinzugsgebiete.component.site-generator :as gen]
+              [clojure.java.io :as io]
+              [duct.core :as duct]
+              [integrant.core :as ig]))
+
+(defn- load-system
+  "Loads the system from the config and initializes it."
+  []
+  (-> "schuleinzugsgebiete/config.edn"
+      (io/resource)
+      (duct/read-config)
+      (duct/prep)
+      (ig/init)))
 
 (defn -main [& args]
-  (duct/exec (duct/read-config (io/resource "schuleinzugsgebiete/config.edn"))))
+  (let [system (load-system)]
+    (duct/add-shutdown-hook ::exec #(ig/halt! system))
+    (let [generator (::gen/generator system)]
+      (generator))))
