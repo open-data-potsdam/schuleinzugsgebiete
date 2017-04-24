c0d65047ef50001e5cc92237359d52cdc3d96966
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2017-04-24 13:39:04 +0200

    Disable template caching in dev mode
    When the layout is changed during development, the pages are still
    served from the cache. Turning caching off works much better during
    development.

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -6,6 +6,7 @@
             [clojure.java.io :as io]
             [duct.core :as duct]
             [eftest.runner :as eftest]
+            [selmer.parser :as templates]
             [integrant.core :as ig]
             [integrant.repl :refer [clear halt go init prep reset]]
             [integrant.repl.state :refer [config system]]))
@@ -23,5 +24,7 @@
   (load "local"))
 
 (integrant.repl/set-prep! (comp duct/prep read-config))
+
+(templates/cache-off!)
 
 (go)
